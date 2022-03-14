b37496316498e1e3522b5026ec77ff1138d0e6d8
Tencho0 <tencho1011@gmail.com>
2022-03-14 22:12:13 +0200

    Changes to project ATM machine

--- ATMMachine/ATMMachine/BankCard.cs
+++ ATMMachine/ATMMachine/BankCard.cs
@@ -8,5 +8,18 @@ namespace ATMMachine
 {
     internal class BankCard
     {
+        public BankCard(int cardNum, string ownerFirstName, string ownerLastName, string PIN)
+        {
+            this.OwnerFirstName = ownerFirstName;
+            this.OwnerLastName = ownerLastName;
+            this.CardNum = cardNum;
+            this.CurrentBalance = 0;
+            this.PIN = PIN;
+        }
+        public string OwnerFirstName { get; set; }
+        public string OwnerLastName { get; set; }
+        public int CardNum { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public string PIN { get; set; }
     }
 }

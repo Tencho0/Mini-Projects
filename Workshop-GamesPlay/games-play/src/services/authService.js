799c076f590a2c4e3df57e05db3a4a0a9ffa198d
Tencho0 <tencho1011@gmail.com>
2023-02-03 21:43:54 +0200

    renamed folder & added contexts to the project

---
+++ Workshop-GamesPlay/games-play/src/services/authService.js
@@ -0,0 +1,4 @@
+import *  as request from "./requester"
+const baseUrl = 'http://localhost:3030';
+
+export const login = (email, password) => request.post(`${baseUrl}/users/login`, { email, password });
---
+++ Workshop-GamesPlay/games-play/src/services/gameService.js
@@ -0,0 +1,5 @@
+import * as request from './requester';
+
+const baseUrl = 'http://localhost:3030';
+
+export const getAll = () => request.get(`${baseUrl}/data/games`);

e9f93c3f5fb985f9beab89b75c42cc3bc6034a0d
Tencho <tencho1011@gmail.com>
2025-01-10 20:02:27 +0300

    Add hackerrank tasks

---
+++ HackerRank/HackerRnak1/BalancedBreckets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Result
+{
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string isBalanced(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char ch in s)
+        {
+            if (ch == '{' || ch == '[' || ch == '(')
+            {
+                stack.Push(ch);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    return "NO";
+                }
+
+                char top = stack.Pop();
+                if ((ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == ')' && top != '('))
+                {
+                    return "NO";
+                }
+            }
+        }
+
+        return stack.Count == 0 ? "YES" : "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.isBalanced(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ HackerRank/p1/p1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace p1
+{
+    public class Person
+    {
+        public string name;
+
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public string SayHello()
+        {
+            string helloMessage = $"Hello, my name is {this.Name} and I am a person";
+
+            return helloMessage;
+
+        }
+
+        public string SayHelloFromManager()
+        {
+            string helloMessage = $"Hello, my name is {this.Name} and I am a manager";
+
+            return helloMessage;
+
+        }
+
+        public string SayHelloFromEmployee()
+        {
+            string helloMessage = $"Hello, my name is {this.Name} and I am a employee";
+
+            return helloMessage;
+
+        }
+
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Peter");
+            string helloMessagePerson = person.SayHello();
+            Console.WriteLine(helloMessagePerson);
+
+            Person employee = new Person("John");
+            string helloMessageEmployee = employee.SayHelloFromEmployee();
+            Console.WriteLine(helloMessageEmployee);
+
+            Person manager = new Person("Bob");
+            string helloMessageManager = manager.SayHelloFromManager();
+            Console.WriteLine(helloMessageManager);
+        }
+
+    }
+
+}

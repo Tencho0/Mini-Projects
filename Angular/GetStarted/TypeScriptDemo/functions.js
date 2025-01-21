6994d476d58de91f0896580f0f156dca076e9897
Tencho <tencho1011@gmail.com>
2025-01-21 19:10:51 +0300

    learn about functions in Typescript

--- Angular/GetStarted/TypeScriptDemo/functions.js
+++ Angular/GetStarted/TypeScriptDemo/functions.js
@@ -3,3 +3,15 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(10, 20));
+const sub = (num1, num2) => num1 - num2;
+console.log(sub(2, 3));
+const mult = function (num1, num2) {
+    return num1 * num2;
+};
+function getItems(items) {
+    return new Array().concat(items);
+}
+let concatResult = getItems([1, 2, 3, 4, 5]);
+let concatStrings = getItems(["A", "B", "C"]);
+console.log(concatResult);
+console.log(concatStrings);
--- Angular/GetStarted/TypeScriptDemo/functions.ts
+++ Angular/GetStarted/TypeScriptDemo/functions.ts
@@ -11,3 +11,14 @@ console.log(sub(2, 3));
 const mult = function (num1: number, num2: number) {
     return num1 * num2;
 }
+
+function getItems<Type>(items: Type[]): Type[] {
+    return new Array<Type>().concat(items);
+}
+
+let concatResult = getItems([1, 2, 3, 4, 5]);
+let concatStrings = getItems(["A", "B", "C"]);
+
+console.log(concatResult);
+console.log(concatStrings);
+

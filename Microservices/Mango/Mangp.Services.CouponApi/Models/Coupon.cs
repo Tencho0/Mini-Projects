42650a5a33145d6dfe24bbc8068224b980a03273
Tencho <tencho1011@gmail.com>
2025-02-12 15:36:39 +0300

    Initialize DB and Seed Coupons

---
+++ Microservices/Mango/Mangp.Services.CouponApi/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Mangp.Services.CouponApi.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Microservices/Mango/Mangp.Services.CouponApi/Models/Dto/CouponDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Mangp.Services.CouponApi.Models.Dto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CouponDto
+    {
+        [Key]
+        public int CouponId { get; set; }
+
+        [Required]
+        public string CouponCode { get; set; }
+
+        [Required]
+        public double DiscountAmount { get; set; }
+
+        public int MinAmount { get; set; }
+    }
+}

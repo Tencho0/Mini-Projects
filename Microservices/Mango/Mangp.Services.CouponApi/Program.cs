a16508dafdc22366ca3491375b0939d8453292c8
Tencho <tencho1011@gmail.com>
2025-02-12 15:41:34 +0300

    Automatically apply pending migration

--- Microservices/Mango/Mangp.Services.CouponApi/Program.cs
+++ Microservices/Mango/Mangp.Services.CouponApi/Program.cs
@@ -27,5 +27,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
 app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}

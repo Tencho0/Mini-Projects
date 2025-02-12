239d323763fb9c5ed1f8fab3aad18182960625cb
Tencho <tencho1011@gmail.com>
2025-02-12 16:02:25 +0300

    Get all coupons and get coupon by id

---
+++ Microservices/Mango/Mangp.Services.CouponApi/Controllers/CouponApiController.cs
@@ -0,0 +1,57 @@
+﻿namespace Mangp.Services.CouponApi.Controllers
+{
+    using Mangp.Services.CouponApi.Data;
+    using Mangp.Services.CouponApi.Models;
+    using Mangp.Services.CouponApi.Models.Dto;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponApiController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}

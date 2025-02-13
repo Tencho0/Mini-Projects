955ebaf70c52a47741c4b8724cafc4b1128bd193
Tencho <tencho1011@gmail.com>
2025-02-13 16:34:00 +0300

    Add roles

--- Microservices/Mango/Mango.Services.AuthApi/Controllers/AuthAPIController.cs
+++ Microservices/Mango/Mango.Services.AuthApi/Controllers/AuthAPIController.cs
@@ -44,5 +44,18 @@
             _response.Result = loginResponse;
             return Ok(_response);
         }
+
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+            var assignRoleSuccessful = await _authService.AssignRole(model.Email, model.Role.ToUpper());
+            if (!assignRoleSuccessful)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Error encountered";
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
     }
 }

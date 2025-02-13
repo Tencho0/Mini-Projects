a03ad96d41c29f045e9161d0cb9e08a8887df462
Tencho <tencho1011@gmail.com>
2025-02-13 13:36:46 +0300

    Create new Auth service

---
+++ Microservices/Mango/Mango.Services.AuthApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Mango.Services.AuthApi.Data
+{
+    using Mango.Services.AuthApi.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Microservices/Mango/Mango.Services.AuthApi/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthApi.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}

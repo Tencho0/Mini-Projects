77aaa07d7dbe950026d6da6f5430ca581dec6426
Tencho <tencho1011@gmail.com>
2025-02-21 17:55:49 +0300

    Create RewardsApi Service

---
+++ Microservices/Mango/Mango.Services.RewardApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Services.RewardApi.Data
+{
+    using Mango.Services.RewardApi.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Rewards> Rewards { get; set; }
+    }
+}

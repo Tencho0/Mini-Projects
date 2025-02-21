506011d7a8132c5bf09af8ffb65846ec1389d49f
Tencho <tencho1011@gmail.com>
2025-02-21 16:50:21 +0300

    Apply Coupons in Stripe

--- Microservices/Mango/Mango.Services.OrderApi/Controllers/OrderApiController.cs
+++ Microservices/Mango/Mango.Services.OrderApi/Controllers/OrderApiController.cs
@@ -70,6 +70,14 @@
                     Mode = "payment",
                 };
 
+                var discountsObj = new List<SessionDiscountOptions>()
+                {
+                    new SessionDiscountOptions
+                    {
+                        Coupon= stripeRequestDto.OrderHeader.CouponCode
+                    }
+                };
+
                 foreach (var item in stripeRequestDto.OrderHeader.OrderDetails)
                 {
                     var sessionLineItem = new SessionLineItemOptions
@@ -87,6 +95,11 @@
                     };
 
                     options.LineItems.Add(sessionLineItem);
+                }
+
+                if (stripeRequestDto.OrderHeader.Discount > 0)
+                {
+                    options.Discounts = discountsObj;
                 }
 
                 var service = new SessionService();

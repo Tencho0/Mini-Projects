f4fa5acb279eb725a324cf9db748aa89b72c2cd1
Tencho <tencho1011@gmail.com>
2025-02-26 11:44:23 +0300

    Move Ocelot Routes into Ocelot Json file

--- Microservices/Mango/Mango.GatewaySolution/Program.cs
+++ Microservices/Mango/Mango.GatewaySolution/Program.cs
@@ -4,10 +4,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
